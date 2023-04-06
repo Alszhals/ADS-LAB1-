5c71d8ce4e7f0e12c3617abab36e4ae34c0fbfd4
LAB1 ADS <asek05052005@gmail.com>
2023-04-06 15:54:55 +0600

    implement

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.Scanner;
 import java.util.stream.*;
@@ -6,9 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-        BigInteger num = BigInteger.valueOf(n);
-        int hepl;
+        findFactorial(n);
 
+    }
+    public static int findFactorial(int n){
+        int fact =1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        System.out.print(fact+" ");
+        return fact;
     }
 }
 
