d17b9c54dcb7d48d5df4364040d2e8fa96a4a56a
LAB1 ADS <asek05052005@gmail.com>
2023-04-09 12:14:14 +0600

    Task2

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
         if(l==0){
             return n[0];
         }
+
         else{
             double avr=findeAverage(n,l-1);
             return IntStream.of(n).average().orElse(Double.NaN);
