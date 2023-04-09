9b2ad657018a58bc656d48c640e5df69ded7454b
Alszhals <113881256+Alszhals@users.noreply.github.com>
2023-04-09 22:12:32 +0600

    Update Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -1,22 +1,31 @@
+/*
+@ n- the number programm must check 
+@findPrime- function which returns true/false 
+@Math.sqrt()-method which gave to programm square root value of our input "n"
+
+
+*/
+
+
 import java.util.Scanner;
 
 public class Task3 {
         public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            int number = input.nextInt();
-            if (isPrime(number)) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if (findPrime(n)) {
                 System.out.println("Prime");
             } else {
                 System.out.println("Composite");
             }
         }
 
-        public static boolean isPrime(int number) {
-            if (number < 2) {
+        public static boolean findPrime(int n) {
+            if (n < 2) {
                 return false;
             }
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
                     return false;
                 }
             }
