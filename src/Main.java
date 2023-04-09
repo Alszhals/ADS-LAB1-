edf3850f695c0efaef65bf6da1428718e06b9515
LAB1 ADS <asek05052005@gmail.com>
2023-04-09 12:12:59 +0600

    implement

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
-import java.sql.SQLOutput;
-import java.util.*;
 import java.util.Scanner;
 import java.util.stream.*;
-import java.math.BigInteger;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        findFactorial(n);
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int in = sc.nextInt();
+        int[] n=new int[in];
+        for(int i=0;i<in;i++){
+            n[i]=sc.nextInt();}
+        double avr=findeAverage(n,n.length);
+        System.out.print(avr);
     }
-    public static int findFactorial(int n){
-        int fact =1;
-        for(int i=1;i<=n;i++){
-            fact=fact*i;
+    public static double findeAverage(int[] n,int l){
+        if(l==0){
+            return n[0];
         }
-        System.out.print(fact+" ");
-        return fact;
+        else{
+            double avr=findeAverage(n,l-1);
+            return IntStream.of(n).average().orElse(Double.NaN);
+        }
+
     }
 }
-
