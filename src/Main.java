300901efeef72cc88f0170960a2208ae7cc7a78f
LAB1 ADS <asek05052005@gmail.com>
2023-04-09 12:16:23 +0600

    Task4

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.*;
 import java.util.Scanner;
 import java.util.stream.*;
+import java.math.BigInteger;
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int in = sc.nextInt();
-        int[] n=new int[in];
-        for(int i=0;i<in;i++){
-            n[i]=sc.nextInt();}
-        double avr=findeAverage(n,n.length);
-        System.out.print(avr);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        findFactorial(n);
+
     }
-    public static double findeAverage(int[] n,int l){
-        if(l==0){
-            return n[0];
-        }
 
-        else{
-            double avr=findeAverage(n,l-1);
-            return IntStream.of(n).average().orElse(Double.NaN);
+    public static int findFactorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
         }
-
+        System.out.print(fact + " ");
+        return fact;
     }
 }
