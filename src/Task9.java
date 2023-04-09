33978e677de94600b7a1f91c6463a2fe45722b5c
LAB1 ADS <asek05052005@gmail.com>
2023-04-09 18:14:20 +0600

    task 9

---
+++ src/Task9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+        System.out.println(binomialCoeff(n, k));
+    }
+
+    public static int binomialCoeff(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        } else {
+            return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
+        }
+    }
+}
+
