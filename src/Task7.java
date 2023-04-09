ce043f8a2fc3f4879b3142fe0745bed561877e36
LAB1 ADS <asek05052005@gmail.com>
2023-04-09 17:38:05 +0600

    task 6

---
+++ src/Task7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int size =sc.nextInt();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = sc.nextInt();
+            }
+            reverseArray(array, 0, size - 1);
+            for (int i = 0; i < size; i++) {
+                System.out.print(array[i] + " ");
+            }
+    }
+
+        public static void reverseArray(int[] array, int startIndex, int endIndex) {
+            if (startIndex >= endIndex) {
+                return;
+            }
+            int temp = array[startIndex];
+            array[startIndex] = array[endIndex];
+            array[endIndex] = temp;
+            reverseArray(array, startIndex + 1, endIndex - 1);
+        }
+    }
+
+
