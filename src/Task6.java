8e3bd6e6a5d98e73e9b76f9595a5562549783c52
LAB1 ADS <asek05052005@gmail.com>
2023-04-09 22:30:21 +0600

    task 6

--- src/Task6.java
+++ src/Task6.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int a = 3;
-            int n = 3;
+            int a =sc.nextInt();
+            int n =sc.nextInt();
             System.out.println( findDegree(a, n));
         }
 
